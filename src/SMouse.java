aff9c7528ffd1adb1867373ee704881430558126
AaronS117 <aaronoropeza98@gmail.com>
2022-09-17 17:51:47 -0500

    Great Success

--- src/SMouse.java
+++ src/SMouse.java
@@ -2,15 +2,14 @@ public class SMouse
 {
     private int age = 0;
     private double weight = 1.0;
-
     public void grow()
     {
         this.weight += 0.2 * this.age;
         ++this.age;
     }
-
     public void display()
     {
-        System.out.println("");
-    }
-}
+        System.out.println("Mouse age: "+ this.age);
+        System.out.println("Mouse weight: "+ this.weight + "\n");
+    }// End Display
+}// End Class
--- src/SMouseDriver.java
+++ src/SMouseDriver.java
@@ -8,16 +8,24 @@ public class SMouseDriver
         SMouse mighty = new SMouse();
         SMouse mickey = new SMouse();
         System.out.println("");
-        System.out.println("");
-        System.out.println("");
-        System.out.print("What age is Mighty mouse: ");
-        int mightAge = stdIn.nextInt();
-        for (int i = 0; i < mightAge; i++) {
-            mighty.grow();
-        }
-        System.out.print("Mighty Mouse age: " + mightAge);
-        System.out.println("Mighty Mouse weight: " + );
-        stdIn.close();
-    }
-}
 
+        // Store age per Mouse individually (Mighty Mouse)
+        System.out.print("What's Mighty Mouse age?: ");
+        int MightAge = stdIn.nextInt();
+        for (int i = 0; i < MightAge; i++) {
+            mighty.grow(); // grow mouse based on age
+        }
+        mighty.display(); // print mouse weight and age
+
+        // Mickey mouse
+        System.out.print("What's Mickey Mouse age?: ");
+        int mickeyAge = stdIn.nextInt();
+        for (int i = 0; i < mickeyAge; i++) {
+            mickey.grow();
+        }
+        mickey.display();
+
+
+        stdIn.close();
+    }// End Main
+}// End Class
